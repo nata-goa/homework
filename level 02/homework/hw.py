629e174e2f8ea3fc6ef93451c276334b78b01415
nata-goa <bajelidzenata0@gmail.com>
2024-12-15 23:31:55 +0400

    third hw

---
+++ level 02/homework/hw.py
@@ -0,0 +1,44 @@
+#ცვლადი არის ადგილი სადაც მონაცემები ინახება
+#ცვლადის სახელი არის  ცვლადის დასახელება fullname
+#ცვლადის მნიშვნელობა არის ის მონაცემი რაც ცვლადში ინახება *Natali bajelidze*
+#ცვლადის სახელი = ცვლადის მნიშვნელობა
+#ცვლადი გამოიყენება იმისთვის რომ ინფორმაცია შევინახოთ მარტივად
+#ცვლადში რასაც შევიყვანთ პრინტშიც შეიცვლება
+
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+print("natali")
+
+fullname = "natali bajelidze"
+print(fullname)
+
+age = 15
+print(age)
+
+height = 168
+print(height)
+
+academy = "goa"
+print("goa")
+
+grade = 9
+print(grade)
+
