f3db8a8c5d9ebbc22614605b975d5f430020f16f
nata-goa <bajelidzenata0@gmail.com>
2025-01-05 21:14:05 +0400

    seventh hw

---
+++ level 06/homework/hw.py
@@ -0,0 +1,39 @@
+#debugging არის კოდის შეცდომების პოვნა და გასწორება
+#case-sensetive ნიშნავს რომ დიდი და პატარა ასოების განსხვავებას მნიშვნელობა აქ
+#case-insensetive ნიშნავს რომ დიდი და პატარა ასოების განსხვავებას მნიშვნელობა არა აქ
+
+#case-insensitive : SQL, Visual Basic, PHP
+#case-sensetive : Linux, Java, C/++
+
+#ეს ოპერატორები ცვლადის ამჟამინდელ მნიშვნელობას გაანახლებენ და შეინახავენ შედეგს იმავე ცვლადში
+
+n = 5
+n += 10
+print(n)
+
+score = 20
+score += 15
+print(score)
+
+num = 4
+num *= 5
+print(num)
+
+value = 20
+value /= 4
+print(value)
+
+remainder = 17
+remainder %= 5
+print(remainder)
+
+power = 3
+power **= 4
+print(power)
+
+
+number = 10
+number += 5
+number *= 2
+number -= 3
+print(number)
---
+++ level 06/homework/hww.py
@@ -0,0 +1,33 @@
+from turtle import *
+#i want to draw a tree
+
+speed(30)
+width(5)
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+left(120)
+forward(50)
+
+speed(30)
+width(5)
+right(60)
+forward(100)
+right(120)
+forward(100)
+right(120)
+forward(100)
+
+penup()
+
+left(120)
+forward(50)
+
+pendown()
+
+
+
+exitonclick()
