2c8dbc075c59d48c661bf12640e8baf25430d1ef
nata-goa <bajelidzenata0@gmail.com>
2025-01-13 00:48:44 +0400

    ninth hw

---
+++ level 08/homework/hw.py
@@ -0,0 +1,18 @@
+#input ინფორმაციის შეყვანა 
+#output თვით მონაცემის შედეგი
+ 
+#input-სთვის გამოიყენება "input() ბრძანება"
+#output-სთვის გამოიყენება "print() ბრძანება"
+
+#type არის ფუნქცია რომლიც საშუალებითაც გავიგებთ ნებისმიერ მონაცემთა ტიპს
+name = "natali"
+print(type(name))
+
+#მონაცემთა ტიპები: integar, float, string, boolean
+
+num1 = 5
+num2 = 10
+num3 = 3
+num4 = 2
+num5 = 4
+print((num1 + num2) / num3 - num4 * num5 + num1**num3 % num2)
