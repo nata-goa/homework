f2e87ba2253ca2ebeead7fea82ab578b71134480
nata-goa <bajelidzenata0@gmail.com>
2025-01-20 00:22:57 +0400

    eleventh hw

---
+++ level 10/homework/hw.py
@@ -0,0 +1,67 @@
+#int მაგალითი
+# a = 5
+# b = 8
+
+#str მაგალითი
+# a = "5"
+# b = "8"
+
+#float მაგალითი
+# a = "5.0"
+# b = "8.1"
+
+#Boolean არის ლოგიკური ტიპი, რომელსაც შეუძლია მიიღოს მხოლოდ ორი მნიშვნელობა
+# True (ჭეშმარიტი)
+# False (მცდარი)
+# == ტოლობა
+# != არატოლობა
+
+#მონაცემთა ტიპის კონვერცია გამოიყენება იმ დროს როცა მონაცემები არის განსხვავებული და მათი გამოყენებისთვის ერთიანი ტიპია საჭირო
+
+#ოპერატორებზე მაგალითები
+
+a = 10
+b = 5
+print(a > b)
+
+a = 5
+b = 10
+print(a < b)
+
+x = 8
+y = 8 
+print (x >= y)
+
+x = 3
+y = 7
+print (x <= y)
+
+a = 12
+b = 10
+print (a != y)
+
+a = 20
+b = 20
+print (a == b)
+
+age = int(input("Enter your age"))
+age_in_10_years = age + 10 
+
+
+num = int(input('Enter your number:'))
+num1 = int(input('Enter your number:'))
+num2 = int(input('Enter your number:'))
+
+print((num + num1 + num2) / 3)
+
+num1 = int(input('Enter your number:'))
+num2 = int(input('Enter your number: '))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+num1 = int(input('Enter your number:'))
+num2 = int(input('Enter your number:'))
+num3 = int(input('Enter your number:'))
