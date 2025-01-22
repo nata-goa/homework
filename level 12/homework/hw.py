02f0964c05883019cfad8ebdce2f4821b0192c64
nata-goa <bajelidzenata0@gmail.com>
2025-01-23 00:12:10 +0400

    twelfth hw

--- level 12/homework/hw.py
+++ level 12/homework/hw.py
@@ -4,3 +4,22 @@ print(num1 < num2 or True or False)
 
 print(num1 > num2 and True and False)
 
+#control flow კოდის შესრულების მართვის პროცესია. მისი 3 ძირითადი ტექნიკაა:
+# Sequencing, Selection, Iteration (მიმდევრობა, არჩევანი, ციკლი)
+
+num1 = 10
+num2 = 28
+
+print(num1 > 5)
+print(num1 < 20)
+print(num1 <= 10)
+print(num1 >= 15)
+print(num1 == 10)
+print(num1 != 5)
+
+print(num1 > num2 and True and False)
+print(num1 < num2 or True or False)
+
+# ალგორითმი ნიშნავს თანმიმდევრობას
+# ერთერთი მაგალითია ჩაის გაკეთება. წყალი აადუღო, ჭიქაში ჩაასხა, ფერი დაუმატო, შაქარი დაუმატო, და მზადაა
+
